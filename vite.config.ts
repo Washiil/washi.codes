805933f5070906274dd1e5e1740c0f7594cf9474
Washi <65116802+Washiil@users.noreply.github.com>
2025-11-20 20:41:57 -0500

    proerly loading css and loading src directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     root: 'src',
     server: {
@@ -17,7 +20,7 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       }
     }
   };
