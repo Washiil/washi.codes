a245a096a08d4b00f3bd1cc3a8b108a3e860d5a2
Washi <65116802+Washiil@users.noreply.github.com>
2025-11-20 19:32:50 -0500

    complete flush of current code

--- src/types/blog.ts
+++
@@ -1,9 +0,0 @@
-export interface BlogPostMetadata {
-  slug: string;
-  title: string;
-  description: string;
-  author: string;
-  date: string;
-  tags: string[];
-  published: boolean;
-}
--- src/types/project.ts
+++
@@ -1,9 +0,0 @@
-export interface ProjectMetadata {
-  slug: string;
-  title: string;
-  description: string;
-  repository: string;
-  url: string;
-  tags: string[];
-  priority: number;
-}
