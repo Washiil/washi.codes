9ef6ec36abc1008c89f145b5bfad49502258a2a1
Washi <65116802+Washiil@users.noreply.github.com>
2025-11-20 20:37:08 -0500

    updated tailwind css packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.some_api_key': JSON.stringify(env.SOME_KEY),
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    root: 'src',
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react(), tailwindcss()],
+    define: {
+      'process.env.some_api_key': JSON.stringify(env.SOME_KEY),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
